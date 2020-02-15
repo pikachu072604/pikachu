17c8443f85fd00a51ba5f718d687ccf6e07237dc
pikachu072604 <60963042+pikachu072604@users.noreply.github.com>
2020-02-15 12:29:43 +0530

    Delete tkintergui2widgetswithpack.py

--- tkintergui2widgetswithpack.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root=Tk()
-def left():
-     print("click!")
-b1=Button(root,text="shravan",command=left)
-b2=Button(root,text="kamat",command=left)
-b1.pack(side=LEFT)
-b2.pack(side=LEFT)
