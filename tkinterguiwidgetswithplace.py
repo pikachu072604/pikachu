402a9219912d2866e8a83291878bcf0eeb73212d
pikachu072604 <60963042+pikachu072604@users.noreply.github.com>
2020-02-15 12:27:40 +0530

    Delete tkinterguiwidgetswithplace.py

--- tkinterguiwidgetswithplace.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root=Tk()
-def callback():
-     print("click!")
-b1=Button(root,text="shravan",command=callback,bg="red",fg="blue")
-b2=Button(root,text="kamat",command=callback,bg="red",fg="blue")
-b1.place(x=200,y=200)
-b2.place(x=500,y=500)
